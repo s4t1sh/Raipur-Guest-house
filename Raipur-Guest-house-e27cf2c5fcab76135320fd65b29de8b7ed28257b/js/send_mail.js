bb3d1711203287a355b177b86321121d485a4a6a
V Satish <vasettisatish@gmail.com>
2023-08-04 15:54:21 +0530

    final

---
+++ Raipur-Guest-house-e27cf2c5fcab76135320fd65b29de8b7ed28257b/js/send_mail.js
@@ -0,0 +1,45 @@
+function send_mail(){
+    window.Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "vasettisatish@gmail.com",
+    Password : "03F085010ED813A909D3EDCEC3FAFBDABCC0",
+    To : 'vasettisatish@gmail.com',
+    From : document.getElementById("email").value,
+    Subject : "New Message from a customer to book "+document.getElementById("farmhouse").value,
+    Body : "Hello Owner," +"<br><br>" + "You got a new message from "+document.getElementById("name").value +" "+ "("+document.getElementById("email").value +"):"+ "<br>"+ "Farmhouse: " + document.getElementById("farmhouse").value+"<br>"+"  Phone Number: "+ document.getElementById("phno").value+"<br>"+" Best Wishes "+"<br>"+document.getElementById("name").value,
+    }).then(
+        document.getElementById("email").value = "",
+        document.getElementById("farmhouse").value ="SELECT FARMHOUSE",
+        document.getElementById("name").value="",
+        document.getElementById("phno").value="",
+        document.getElementById("alert1").style.display="block"
+    );
+   
+}
+
+function send_mail_bottom(){
+    window.Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "vasettisatish@gmail.com",
+    Password : "03F085010ED813A909D3EDCEC3FAFBDABCC0",
+    To : 'vasettisatish@gmail.com',
+    From : document.getElementById("email_bottom").value,
+    Subject : "New Message from a customer to book "+document.getElementById("farmhouse_bottom").value,
+    Body : "Hello Owner," +"<br><br>" + "You got a new message from "+document.getElementById("name_bottom").value +" "+ "("+document.getElementById("email_bottom").value +"):"+ "<br>"+ "Farmhouse: " + document.getElementById("farmhouse_bottom").value+"<br>"+"  Phone Number: "+ document.getElementById("phno_bottom").value+"<br>"+" Best Wishes "+"<br>"+document.getElementById("name_bottom").value,
+    }).then(
+        document.getElementById("email_bottom").value = "",
+        document.getElementById("farmhouse_bottom").value ="SELECT FARMHOUSE",
+        document.getElementById("name_bottom").value="",
+        document.getElementById("phno_bottom").value="",
+        document.getElementById("alert2").style.display="block"
+    );
+    
+}
+
+function btn_close() {
+    document.getElementById("alert1").style.display="none";    
+}
+
+function btn_close_bottom() {
+    document.getElementById("alert2").style.display="none";
+}
